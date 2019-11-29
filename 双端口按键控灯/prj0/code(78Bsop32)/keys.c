8b51d7c92784ed0ea379eba46e1fe4a06638164e
WIN10-20180312W\Administrator <2604439872@qq.com>
2019-11-29 10:35:11 +0800

    看门狗清零

--- 双端口按键控灯/prj0/code(78Bsop32)/keys.c
+++ 双端口按键控灯/prj0/code(78Bsop32)/keys.c
@@ -2,12 +2,7 @@
 #include "includeAll.h"
 //=============================================================================
 void GetKeys() {
-  static uint8_t tmpKeyValue = D_keyNull;
-
   if (P_key1 == 0) {
-    tmpKeyValue = D_keyValue1;
-  } else {
-    keyValue = tmpKeyValue;
-    tmpKeyValue = D_keyNull;
+    keyValue = D_keyValue1;
   }
 }
